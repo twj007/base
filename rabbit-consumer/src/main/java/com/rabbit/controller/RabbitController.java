3ce9a7d279c2dce07c46651a57e59b166d4a2b5c
twj007 <530747628@qq.com>
2019-06-12 17:59:58 +0800

    增加rabbitmq 消费者

---
+++ rabbit-consumer/src/main/java/com/rabbit/controller/RabbitController.java
@@ -0,0 +1,15 @@
+package com.rabbit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ **@project: base
+ **@description:
+ **@Author: twj
+ **@Date: 2019/06/12
+ **/
+@RestController
+public class RabbitController {
+
+
+}
--- web/src/main/java/com/framework/web/dao/WebDao.java
+++ web/src/main/java/com/framework/web/dao/WebDao.java
@@ -1,9 +1,11 @@
 package com.framework.web.dao;
 
 import com.framework.web.pojo.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface WebDao {
 
     List<User> listUser();
